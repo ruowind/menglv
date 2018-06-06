cd0a7e9d2f11248b88dbe2f67971925b1889b87a
ruowind <ruowind@outlook.com>
2018-06-06 17:27:04 +0800

    dsfdfdf

--- src/render/App.vue
+++ src/render/App.vue
@@ -3,16 +3,27 @@
     <top-bar></top-bar>
     <div class="projects">
       <div class="projects-header">项目列表</div>
-      <ul class="projects__list" id="js-project-list">
-        <li class="projects__list-item projects__list-item_current projects__list-item_watching" data-project="tmh_list" title="E:\test\tmh_list\tmh_list">
+      <div class="projects__list" id="js-project-list">
+
+        <!-- <div class="projects__list-item projects__list-item_current projects__list-item_watching" data-project="tmh_list" title="E:\test\tmh_list\tmh_list">
           <span class="icon icon-finder" data-finder="true" title="在 文件夹 中查看"></span>
           <div class="projects__list-content">
             <span class="projects__name">tmh_list</span>
             <div class="projects__path">E:\test\tmh_list\tmh_list</div>
           </div>
           <a href="javascript:;" class="icon icon-info projects__info" title="项目设置"></a>
-        </li>
-      </ul>
+        </div> -->
+
+        <div v-for="{p,key} in projects" v-bind:key="key" v-bind:class="getProjectClass(key)" v-bind:title="p.remoteObj.path">
+          <span class="icon icon-finder" data-finder="true" title="在 文件夹 中查看"></span>
+          <div class="projects__list-content">
+            <span class="projects__name">{{p.remoteObj.name}}</span>
+            <div class="projects__path">{{p.remoteObj.path}}</div>
+          </div>
+          <a href="javascript:;" class="icon icon-info projects__info" title="项目设置"></a>
+        </div>
+
+      </div>
     </div>
     <div class="logs logs_show" id="js-log">
       <span class="icon icon-clean" id="js-clean-log" title="清除日志"></span>
@@ -41,28 +52,80 @@
 </template>
 
 <script>
-import topBar from './components/topBar.vue'
+import topBar from "./components/topBar.vue";
 
 export default {
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      msg: "Welcome to Your Vue.js App",
+      projects: {}
+    };
   },
   components: {
     topBar
   },
   methods: {
-    aaa() {
-      Electron.remote.BrowserWindow.getFocusedWindow().minimize();
+    // aaa() {
+    //   Electron.remote.BrowserWindow.getFocusedWindow().minimize();
+    // },
+    getProjectClass(id) {
+      console.log(id)
+      let classes = [];
+      classes.push("projects__list-item");
+      this.projects[id].remoteObj.current && classes.push("projects__list-item_current");
+      this.projects[id].remoteObj.watching &&
+        classes.push("projects__list-item_watching");
+      return classes;
     }
   },
-  mounted(){
-    Electron.ipcRenderer.send('aa', 'ping')
+  mounted() {
+    this.projects = {
+      a: {
+        svnPage: "",
+        svnStatic: "",
+        livereload: true,
+        reversion: true,
+        compress: false,
+        support2x: true,
+        jsPath: "js",
+        cssPath: "css",
+        lessPath: "less",
+        sassPath: "sass",
+        imgPath: "images",
+        projectPath: "searchList/tuangou",
+        remoteObj: {
+          name: "Test",
+          path: "E:\\test\\tmh_list\\tmh_list",
+          current: false,
+          watching: false
+        }
+      },
+      b: {
+        svnPage: "",
+        svnStatic: "",
+        livereload: true,
+        reversion: true,
+        compress: false,
+        support2x: true,
+        jsPath: "js",
+        cssPath: "css",
+        lessPath: "less",
+        sassPath: "sass",
+        imgPath: "images",
+        projectPath: "searchList/tuangou",
+        remoteObj: {
+          name: "Test12",
+          path: "E:\\test\\tmh_list\\tmh_list",
+          current: true,
+          watching: false
+        }
+      }
+    };
+    // Electron.ipcRenderer.send("aa", "ping");
   }
-}
+};
 </script>
 
 <style lang="scss">
-@import './styles/index';
+@import "./styles/index";
 </style>
--- src/render/components/topBar.vue
+++ src/render/components/topBar.vue
@@ -9,50 +9,51 @@
 
 <script>
 export default {
-    methods: {
-        minWindow() {
-            Electron.remote.BrowserWindow.getFocusedWindow().minimize();
-        },
-        closeWindow() {
-            Electron.remote.BrowserWindow.getFocusedWindow().close();
-        }
+  methods: {
+    minWindow() {
+      Electron.remote.BrowserWindow.getFocusedWindow().minimize();
+    },
+    closeWindow() {
+      Electron.remote.BrowserWindow.getFocusedWindow().close();
     }
-}
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 .top-bar {
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 30px;
-    box-sizing: border-box; // background-color: #5ab3ff;
-    -webkit-app-region: drag;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 30px;
+  box-sizing: border-box; // background-color: #5ab3ff;
+  -webkit-app-region: drag;
 }
 
 .btns {
-    float: right;
-    -webkit-app-region: no-drag;
-    a {
-        display: block;
-        width: 30px;
-        height: 30px;
-        float: left;
-        transition: all .5s;
-        cursor: pointer;
+  float: right;
+  -webkit-app-region: no-drag;
+  a {
+    display: block;
+    width: 30px;
+    height: 30px;
+    float: left;
+    transition: all 0.5s;
+    cursor: pointer;
+  }
+  .min-btn {
+    background: url(../assets/minbutton_normal.png) center;
+    &:hover {
+      background-color: rgba(255, 255, 255, 0.2);
     }
-    .min-btn {
-        background: url(../assets/minbutton_normal.png) center;
-        &:hover {
-            background-color: rgba(255, 255, 255, .2)
-        }
+  }
+  .close-btn {
+    background: url(../assets/closebutton_normal.png) center;
+    border-top-right-radius: 2px;
+    &:hover {
+      background-color: #fb6155;
     }
-    .close-btn {
-        background: url(../assets/closebutton_normal.png) center;
-        &:hover {
-            background-color: #FB6155;
-        }
-    }
+  }
 }
 </style>
